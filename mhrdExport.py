6f7eca0098342df4627581092d4cb0ce547941a3
Edward Barak <edward.barak@gmail.com>
2018-07-16 08:58:59 -0700

    Refined regex pattern.

--- mhrdExport.py
+++ mhrdExport.py
@@ -1,18 +1,23 @@
 import codecs
 import string
-# import re
+import re
 
 file = codecs.open('savestate', 'r', encoding='utf-8', errors='ignore')
 savestate = file.read()
 file.close()
 
 printable = set(string.printable)
-ssClean = ''.join(filter(lambda x: x in printable, savestate))
+savestate_cleaned = ''.join(filter(lambda x: x in printable, savestate))
 
-ptn = re.compile('ppt\t?(.*?)sq~')
-re.findall(ptn, ssClean)
+re.findall(ptn, savestate_cleaned)
 
-fa = re.compile('ppt.*?^[A-Z]([A-Z]*?)sq~pt'); re.findall(fa, ssClean)
+ptn = re.compile('(?s)([A-Z0-9]*?)sq~pt.*?(Inputs:.*?Wires:.*?;)') # Misses RAM4W16B
+gates = re.findall(ptn, savestate_cleaned)
 
+# for gate in gates:
+    # create file named gate[0].txt
+    # export gate[1] into gate[0].txt
+
+# ETL: Extract & Transform gate RAM4W16B, and Load into RAM4W16B.txt. Do this because the main pattern misses the RAM4W16B gate due to savestate data structure irregularity. 
 
 
